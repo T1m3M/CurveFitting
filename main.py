9e65e5a2b4396f03e333afe1f7b2e8441f77f2b7
T1m3-m4ch1n3 <abdelrahman_nasr99@hotmail.com>
2021-11-22 09:49:32 +0200

    Adding cases parsing

--- main.py
+++ main.py
@@ -22,3 +22,29 @@ class Case:
 
     def getYPoints(self):
         return [point.y for point in self.points]
+
+
+def cases_parsing(file_data, total_cases):
+    counter = 0
+    cases = []
+
+    for _ in range(total_cases):
+        first_line = file_data[counter].split()
+
+        num_of_points = int(first_line[0])
+        polynomial_degree = int(first_line[1])
+
+        case = Case(num_of_points, polynomial_degree)
+
+        counter += 1
+
+        # reading points for each case
+        for i in range(num_of_points):
+            point_line = file_data[counter + i].split()
+            point = Point(float(point_line[0]), float(point_line[1]))
+            case.addPoint(point)
+
+        counter += num_of_points
+        cases.append(case)
+
+    return cases
